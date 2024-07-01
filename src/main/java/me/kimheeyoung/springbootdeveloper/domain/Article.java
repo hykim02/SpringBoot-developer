a6ddbc1d3998730920d3288a643cdef58d579cd7
hykim02 <hykim4435@naver.com>
2024-07-02 02:01:34 +0900

    post api 작성완료

---
+++ src/main/java/me/kimheeyoung/springbootdeveloper/domain/Article.java
@@ -0,0 +1,29 @@
+package me.kimheeyoung.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+    @Id // 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 생성 방법 (AUTO_INCREMENT)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false) // title이라는 not null 컬럼과 매핑
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder // 빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
